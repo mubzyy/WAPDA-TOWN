bd3af21d3be21ba8a0f9f836cc0770d5b94915e2
Shahbaz Sabir <2412473@szabist-isb.pk>
2026-06-29 15:23:21 +0500

    Testing

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,30 @@ import ManagerMember from "./Pages/ManagerMember";
 import AllottProperty from "./Pages/AllottProperty";
 import UpdatePayment from "./Pages/UpdatePayment";
 import PropertyHistory from "./Pages/PropertyHistory";
-import  MainPage from "./Pages/MainPage"
+import MainPage from "./Pages/MainPage"
 import Loader from "./MainPage/Loader";
 
 
 const App = () => {
-  return (  
-//     <div>
-// <MainPage/>
-//     </div>
-     <Loader>
-        <Routes>
+  return (
+    //     <div>
+    // <MainPage/>
+    //     </div>
+    <Loader>
+      <Routes>
         <Route path="/" element={<ManagerMember />} />
         <Route path="/members" element={<ManagerMember />} />
         <Route path="/allottproperty" element={<AllottProperty />} />
         <Route path="/updatepayment" element={<UpdatePayment />} />
         <Route path="/propertyhistory" element={<PropertyHistory />} />
+        {/* test */}
         {/* <Route path="/" element={<Navigate to="/transfer-property" replace />} /> */}
-      {/* <Route path="/dashboard" element={<Login />} />
+        {/* <Route path="/dashboard" element={<Login />} />
        <Route path="/manage-member" element={<ManageMemberAction />} />
         <Route path="/transfer-property" element={<TransferProperty />} />
          <Route path="*" element={<Navigate to="/transfer-property" replace />} /> */}
       </Routes>
-    </Loader> 
+    </Loader>
   );
 };
 
