a8ead5b160b5ea1f09259dcf0663d81f6785349c
mubzyy <mubashirmustafakhan1010@gmail.com>
2026-07-01 11:53:49 +0500

    Latest push 2

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
 
   <StrictMode>
-      <BrowserRouter>
-    <App />
-    </BrowserRouter>
+      <BrowserRouter basename="/WAPDA-TOWN">
+        <App />
+      </BrowserRouter>
   </StrictMode>,
 )
